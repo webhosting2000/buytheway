affcd12426ec54a5e61852d1cc8627c7b5780cba
webhosting2000 <projektowaniestron0@wp.pl>
2026-05-23 18:43:09 +0200

    Add files via upload

---
+++ roza/mail/contact_me.php
@@ -0,0 +1,25 @@
+﻿<?php
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['tel3'])     ||
+   empty($_POST['message2'])   ||
+   !empty($_POST['content2'])  ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$message = strip_tags(htmlspecialchars($_POST['message2']));
+
+// Create the email and send the message
+$to = 'gregory35@wp.pl';
+$email_subject = "Kwiaciarnia - Wiadomość ze strony internetowej:  $name";
+$email_body = "Kwiaciarnia - Wiadomość ze strony internetowej.\n\nImię: $name\n\nEmail: $email_address\n\nWiadomość:\n$message";
+$headers = "Reply-To: $email_address";
+
+mail($to,$email_subject,$email_body,$headers);
+return true;
+?>
